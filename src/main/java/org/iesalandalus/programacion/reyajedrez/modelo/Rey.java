ca5be6fbc5417b308eab4e24cb2220ab3b5efd3e
Daniel888Mendez <linoalmeria2@gmail.com>
2023-11-13 22:35:05 +0100

    clase rey

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -5,4 +5,23 @@ public class Rey {
     Posicion posicion;
     private int totalMovimientos;
 
+    public Color getColor() {
+        return color;
+    }
+
+    private void setColor(Color color) {
+        if (color==Color.NEGRO || color==Color.BLANCO){
+            this.color=color;
+        }else
+            throw new IllegalArgumentException("Error:El color es erroneo");
+
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+
+
+
 }
